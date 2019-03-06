4a7ae214a68a8a0966741002dff9446a3ceecc1b
mlnlmsl <melamsal555@gmail.com>
2019-03-06 23:52:19 +0545

    Initial work on the view of the user and project

--- projecthunt/settings.py
+++ projecthunt/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user.app.UserConfig',
-    'project.app.ProjectConfig',
+    'user',
+    'project',
 ]
 
 MIDDLEWARE = [
--- projecthunt/urls.py
+++ projecthunt/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+import user
+import project
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('project/', include('project.urls')),
 ]
