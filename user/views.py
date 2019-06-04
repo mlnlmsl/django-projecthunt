8d188284291a7cd08588632d5acd8984ff8ffa64
mlnlmsl <melamsal555@gmail.com>
2019-06-04 08:22:41 +0545

    minor changes

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.http import HttpResponse
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -8,9 +9,15 @@ def index(request):
 
 
 def register(request):
-    # form = UserCreationForm()
+    form = UserCreationForm()
     if request.method == 'GET':
-        return render(request, 'user/register.html')
+        return render(request, 'user/register.html',{'form':form})
     elif request.method == "POST":
-        print(request.POST)
-        return  HttpResponse("<p> register post</p>")
+        username = request.POST['username']
+        email = request.POST['email']
+        passowrd = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if passowrd != confirm_password:
+            messages.error(request, 'Password must match')
+
+        return HttpResponse("<p> register post</p>")
